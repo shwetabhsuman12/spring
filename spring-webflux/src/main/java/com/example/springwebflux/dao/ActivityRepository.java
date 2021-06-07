b83a22407a1c688ebf35e3e833d661c22cee956d
shwetabh <shwetabhpoc@gmail.com>
2021-06-07 18:44:54 +0530

    added boolean  while saving to check the state.

--- spring-webflux/src/main/java/com/example/springwebflux/dao/ActivityRepository.java
+++ spring-webflux/src/main/java/com/example/springwebflux/dao/ActivityRepository.java
@@ -1,9 +1,12 @@
 package com.example.springwebflux.dao;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.core.ReactiveSetOperations;
 import org.springframework.data.redis.core.ReactiveValueOperations;
 import org.springframework.stereotype.Repository;
 
@@ -22,13 +25,14 @@ public class ActivityRepository{
 		public void save(HelloWorld actDtl)
 		{
 			ReactiveValueOperations<String,String> valueOper=operatons.opsForValue();
-	        valueOper.set(actDtl.getName(),actDtl.getGreetMsg());
-	
+	        Mono<Boolean> b=valueOper.set(actDtl.getName(),actDtl.getGreetMsg());
+	        b.subscribe(result -> System.out.println(result));
 		}
 		
 		public Mono<HelloWorld> getMessageByName(String name)
 		{
 		Mono<HelloWorld> hello= operatons.opsForValue().get(name).map(result -> new HelloWorld(name,result));
+	    hello.subscribe(result -> System.out.println("Name fetched:"+result.getName())).toString();
 		
 		return hello;
 		}
